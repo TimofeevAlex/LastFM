326ccc0123ba89215b1396b8ca2086c08c2318c6
Nat998 <31179705+Nat998@users.noreply.github.com>
2021-05-18 11:00:58 +0200

    add hit rate

--- src/metrics.py
+++ src/metrics.py
@@ -17,3 +17,33 @@ def precision_recall_at_k(test, pred_ratings, k=10):
         precisions.append(len(top_k & test_artists) / float(k))
         recalls.append(len(top_k & test_artists) / len(test_artists))
     return precisions, recalls
+
+def hit_rate_user(user, top_recs, test):
+    '''
+    - top_recs: list of top reccommendations for the given user
+    '''
+    num_hits = 0
+    df_user = test.loc[test['user_email'] == user]
+   # print('user = {}'.format(user))
+    for artist in top_recs:
+     # print('artist = {}'.format(artist))
+      df_user_artist = df_user.loc[df_user['artist_id'] == artist]
+      
+      if((not df_user_artist.empty)):
+        if (df_user_artist.iloc[0]['log_plays'] > 0):
+         # print('second if')
+          num_hits += 1
+
+    return num_hits
+
+def hit_rate_total(test, recs):
+    '''
+    - recs: dict
+        Keys are users and values are lists of top reccommendations for the user
+    '''
+    users = test['user_email'].unique()
+    total_hits = 0
+    for user in users:
+        total_hits += hit_rate_user(user, recs[user], test)
+    
+    return total_hits/test['user_email'].nunique()
