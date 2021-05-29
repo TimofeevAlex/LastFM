7fd0944aae3c2fe89dcb791d3542e93450a9b0a7
TimofeevAlex <timofeev.am@phystech.edu>
2021-05-29 16:35:43 +0200

    Add extra experiments for NeuMF

--- src/models.py
+++ src/models.py
@@ -10,7 +10,63 @@ def baseline_predict(data, k=100):
     ranked_artist_scores = total_log_plays.sort_values(['rating'], ascending=False)['rating']
     return ranked_artist_scores[:k]
 
-def create_ncf_model(num_factors, num_user_features, num_artists, num_users, reg=0.01):
+def create_shallow_model(num_factors, num_users, num_artists):
+    # users
+    user_email = tf.keras.layers.Input(shape = [1], name = 'user_email')
+    user_matrix = tf.keras.layers.Embedding(num_users+1, num_factors, name = 'user_matrix')(user_email)
+    user_vector = tf.keras.layers.Flatten(name = 'user_vector')(user_matrix)
+    
+    # artists
+    artist_id = tf.keras.layers.Input(shape = [1], name = 'artist_id')
+    artist_matrix = tf.keras.layers.Embedding(num_artists+1, num_factors, name = 'artist_matrix')(artist_id)
+    artist_vector = tf.keras.layers.Flatten(name = 'artist_vector')(artist_matrix)
+    
+    # dot product
+    vector_product = tf.keras.layers.dot([user_vector, artist_vector], axes = 1, normalize = False)
+    
+    # model
+    model = tf.keras.models.Model(inputs = [user_email, artist_id], outputs = [vector_product], name = 'shallow_model')
+    
+    return model
+
+def create_neumf_model(num_factors, num_artists, num_users, reg=0.01):
+    # User IDs branch
+    user_id = tf.keras.layers.Input(shape=[1], name='user_id')
+    user_matrix_1 = tf.keras.layers.Embedding(num_users+1, num_factors // 2, name='user_matrix_1', embeddings_regularizer=l2(reg))(user_id)
+    user_matrix_2 = tf.keras.layers.Embedding(num_users+1, num_factors // 2, name='user_matrix_2', embeddings_regularizer=l2(reg))(user_id)
+    user_vector_proc_1 = tf.keras.layers.Flatten(name='user_id_vector_1')(user_matrix_1)
+    user_vector_proc_2 = tf.keras.layers.Flatten(name='user_id_vector_2')(user_matrix_2)
+    
+    # Item IDs branch
+    artist_id = tf.keras.layers.Input(shape=[1], name='artist_id')
+    artist_matrix_1 = tf.keras.layers.Embedding(num_artists+1, num_factors, name='artist_matrix_1', embeddings_regularizer=l2(reg))(artist_id)
+    artist_matrix_2 = tf.keras.layers.Embedding(num_artists+1, num_factors, name='artist_matrix_2', embeddings_regularizer=l2(reg))(artist_id)
+    artist_vector_proc_1 = tf.keras.layers.Flatten(name='artist_vector_1')(artist_matrix_1)
+    artist_vector_proc_2 = tf.keras.layers.Flatten(name='artist_vector_2')(artist_matrix_2)
+    
+    # Concantenation and multiplication
+    vectors_mult = tf.keras.layers.Multiply()([user_vector_proc_1, artist_vector_proc_1]) #
+    vectors_concat = tf.keras.layers.Concatenate()([user_vector_proc_2, artist_vector_proc_2]) #
+    vectors_concat_dropout = tf.keras.layers.Dropout(0.5)(vectors_concat)
+    
+    # Backbone 
+    dense_1 = tf.keras.layers.Dense(2 * num_factors, name='fc1', activation='relu', kernel_regularizer=l2(reg))(vectors_concat_dropout)
+    dropout_1 = tf.keras.layers.Dropout(0.5, name='d1')(dense_1)
+    dense_2 = tf.keras.layers.Dense(num_factors, name='fc2', activation='relu', kernel_regularizer=l2(reg))(dropout_1)
+    dropout_2 = tf.keras.layers.Dropout(0.5, name='d2')(dense_2)
+    dense_3 = tf.keras.layers.Dense(num_factors // 2, name='fc3', activation='relu', kernel_regularizer=l2(reg))(dropout_2)
+    dense_4 = tf.keras.layers.Dense(num_factors // 4, name='fc4', activation='relu', kernel_regularizer=l2(reg))(dense_3)
+
+    # Merging a processed concatenated vector and a multiplication result 
+    vectors_merged = tf.keras.layers.Concatenate()([vectors_mult, dense_4])
+    output = tf.keras.layers.Dense(1, activation='sigmoid', use_bias=False,
+                                   name='output', kernel_initializer="lecun_uniform", kernel_regularizer=l2(reg))(vectors_merged)
+    
+    # Model definition
+    model = tf.keras.models.Model(inputs=[user_id, artist_id], outputs=[output], name='deep_factor_model')#
+    return model
+
+def create_neumf_model_si(num_factors, num_user_features, num_artists, num_users, reg=0.01):
     # User IDs branch
     user_id = tf.keras.layers.Input(shape=[1], name='user_id')
     user_matrix_1 = tf.keras.layers.Embedding(num_users+1, num_factors // 2, name='user_matrix_1', embeddings_regularizer=l2(reg))(user_id)
@@ -51,23 +107,4 @@ def create_ncf_model(num_factors, num_user_features, num_artists, num_users, reg
     
     # Model definition
     model = tf.keras.models.Model(inputs=[user_id, user_feats, artist_id], outputs=[output], name='deep_factor_model')#
-    return model
-
-def create_shallow_model(num_factors, num_users, num_artists):
-    # users
-    user_email = tf.keras.layers.Input(shape = [1], name = 'user_email')
-    user_matrix = tf.keras.layers.Embedding(num_users+1, num_factors, name = 'user_matrix')(user_email)
-    user_vector = tf.keras.layers.Flatten(name = 'user_vector')(user_matrix)
-    
-    # artists
-    artist_id = tf.keras.layers.Input(shape = [1], name = 'artist_id')
-    artist_matrix = tf.keras.layers.Embedding(num_artists+1, num_factors, name = 'artist_matrix')(artist_id)
-    artist_vector = tf.keras.layers.Flatten(name = 'artist_vector')(artist_matrix)
-    
-    # dot product
-    vector_product = tf.keras.layers.dot([user_vector, artist_vector], axes = 1, normalize = False)
-    
-    # model
-    model = tf.keras.models.Model(inputs = [user_email, artist_id], outputs = [vector_product], name = 'shallow_model')
-    
     return model
---
+++ src/neumf_helpers.py
@@ -0,0 +1,97 @@
+import numpy as np
+# import pandas as pd
+import tensorflow as tf
+from tqdm import tqdm
+
+def create_train_step(model, optimizer, loss_fn, epoch_loss_avg, epoch_rmse):
+    @tf.function
+    def train_step(x, y, weights):
+        with tf.GradientTape() as tape:
+            logits = model(x, training=True)
+            loss_value = loss_fn(y, logits, sample_weight=weights)
+        grads = tape.gradient(loss_value, model.trainable_weights)
+        optimizer.apply_gradients(zip(grads, model.trainable_weights))
+        epoch_loss_avg.update_state(loss_value)
+        epoch_rmse.update_state(y, logits)
+    return train_step
+
+def create_test_step(model, loss_fn, test_loss_avg, test_rmse):
+    @tf.function
+    def test_step(x, y):
+        logits = model(x, training=False)
+        loss_value = loss_fn(y, logits)
+        test_loss_avg.update_state(loss_value)
+        test_rmse.update_state(y, logits)
+        return logits
+    return test_step
+
+def create_inference(model):
+    @tf.function(experimental_relax_shapes=True)
+    def inference(x):
+        logits = model(x, training=False)
+        return logits
+    return inference
+
+def train_one_epoch(train_step, train_dataset, epoch_loss_avg, epoch_rmse, si=True):
+    epoch_loss_avg.reset_states()
+    epoch_rmse.reset_states()
+    for batch in train_dataset:
+        user_id = batch[:, 0]
+        if si:
+            user_feats = lastfm_360_demo.loc[user_id]
+        artist_id = batch[:, 1]
+        y = batch[:, 2]
+        weights = tf.expand_dims(1. + 0.25*y, -1)##weights from another method, another strategy of uncertainty, SGD/Adam
+        if si:
+            train_step([user_id, user_feats, artist_id], y >= threshold, weights) #user_feats, 
+        else:
+            train_step([user_id, artist_id], y >= threshold, weights)
+    return epoch_loss_avg.result().numpy(), epoch_rmse.result().numpy()
+
+def validate_one_epoch(val_step, valid_dataset, val_loss_avg, val_rmse, si=True):
+    val_loss_avg.reset_states()
+    val_rmse.reset_states()
+    for batch in valid_dataset:
+        user_id = batch[:, 0]
+        if si:
+            user_feats = lastfm_360_demo.loc[user_id]
+        artist_id = batch[:, 1]
+        y = batch[:, 2]
+        if si:
+            val_step([user_id, user_feats, artist_id], y >= threshold) 
+        else:
+            val_step([user_id, artist_id], y >= threshold) 
+    return val_loss_avg.result().numpy(), val_rmse.result().numpy()
+
+def test_one_epoch(test_step, test_dataset, test_loss_avg, test_rmse, si=True):
+    test_loss_avg.reset_states()
+    test_rmse.reset_states()
+    probs = np.array([])
+    for batch in test_dataset:
+        user_id = batch[:, 0]
+        if si:
+            user_feats = lastfm_360_demo.loc[user_id]
+        artist_id = batch[:, 1]
+        y = batch[:, 2]
+        if si:
+            probs_batch = test_step([user_id, user_feats, artist_id], y >= threshold)
+        else:
+            probs_batch = test_step([user_id, artist_id], y >= threshold)
+        probs = np.append(probs, tf.squeeze(probs_batch).numpy())
+    return probs, test_loss_avg.result().numpy(), test_rmse.result().numpy()
+
+def plot_metrics(train_loss_results, train_rmse_results, val_loss_results, val_rmse_results, log_frequency, timenow):
+    fig, axes = plt.subplots(2, sharex=True, figsize=(12, 8))
+    fig.suptitle('Training Metrics')
+
+    axes[0].set_ylabel("Loss", fontsize=14)
+    axes[0].plot(train_loss_results, label='Train')
+    axes[0].plot(np.arange(0, epoch+1, log_frequency), val_loss_results, label='Validation')
+
+    axes[1].set_ylabel("RMSE", fontsize=14)
+    axes[1].set_xlabel("Epoch", fontsize=14)
+    axes[1].plot(train_rmse_results, label='Train')
+    axes[1].plot(np.arange(0, epoch+1, log_frequency), val_rmse_results, label='Validation')
+    plt.legend()
+    plt.savefig('plots/metrics_'+ timenow +'.png')
+    plt.show()
